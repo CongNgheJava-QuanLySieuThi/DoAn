c0e5704e1c00be3e2f703027504487fe7cce147b
chocopro1123 <narizmusicrl@gmail.com>
2024-05-06 13:41:48 +0700

    Test GUI
    Test thử phần giao diện xem có ổn không

---
+++ DoAn/src/GUI/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import GUI.View.MainPage;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    public static void main(String[] args) {
+        new MainPage().setVisible(true);
+    }
+}
